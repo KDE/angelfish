9331f3f27811bfa509ca8f18f974b71013aa58b2
Jonah Brüchert <jbb@kaidan.im>
2021-06-09 22:23:54 +0200

    Add readerview class

---
+++ src/readerview.h
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2021 Jonah Brüchert <jbb@kaidan.im>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <QObject>
+
+class ReaderView {
+    Q_GADGET
+
+    Q_PROPERTY(QString title MEMBER m_title CONSTANT)
+    Q_PROPERTY(QString content MEMBER m_content CONSTANT)
+
+public:
+    ReaderView(QString title, QString content);
+
+private:
+    QString m_title;
+    QString m_content;
+};
+
+class ReaderViewExtractor : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit ReaderViewExtractor(QObject *parent = nullptr);
+
+    ReaderView *extractContent(const QString &originalHtml, const QString &sourceUrl);
+};
+
