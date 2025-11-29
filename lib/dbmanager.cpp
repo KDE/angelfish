305458b1c896d9e87d35ce6e1113cfa65e1bd61c
Rinigus Saar <rinigus.git@gmail.com>
2025-11-29 12:00:29 +0200

    Fix update of the last visit

--- lib/dbmanager.cpp
+++ lib/dbmanager.cpp
@@ -128,7 +128,7 @@ QCoro::Task<> DBManager::setLastVisitedRecord(const QString table, const QString
         co_return;
 
     const qint64 lastVisited = QDateTime::currentSecsSinceEpoch();
-    co_await m_database->execute(QStringLiteral("UPDATE %1 SET lastVisited = ? WHERE url = ?").arg(table), url, lastVisited);
+    co_await m_database->execute(QStringLiteral("UPDATE %1 SET lastVisited = ? WHERE url = ?").arg(table), lastVisited, url);
 
     Q_EMIT databaseTableChanged(table);
 }
